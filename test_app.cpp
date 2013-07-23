bc0228c27073c13200042dbf3627e7ceafd9a90f
amaxilat <null>
2013-07-23 10:32:39 -0700

    change2

--- test_app.cpp
+++ test_app.cpp
@@ -13,7 +13,8 @@ class ExampleApplication
     
         void init( Os::AppMainParameter& value )
         {
-        	
+        	change1
+        	change1
         	
             radio_ = &wiselib::FacetProvider<Os, Os::Radio>::get_facet( value );
             timer_ = &wiselib::FacetProvider<Os, Os::Timer>::get_facet( value );
